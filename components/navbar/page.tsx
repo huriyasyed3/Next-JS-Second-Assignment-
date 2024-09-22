bdd7fa0e9064a1afb7be016f97c074708d756690
Huriya Syed <huriyasyed462@gmail.com>
2024-09-22 23:41:34 +0500

    first commit

---
+++ components/navbar/page.tsx
@@ -0,0 +1,14 @@
+function navbar () {
+    return <div>
+    <div>
+     <h1>Navbar</h1>
+     <ul>
+     <a href="">project</a>
+     <a href="">About</a>
+     <a href="">Projects</a>
+     <a href="">Contact</a>
+     </ul>
+    </div>
+    </div>
+}
+export navbar
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./Components/Header/header";
+import About from "./Components/About/about";
+
+export default function  HomePage(){
+  return <div>
+         <h1>This is Home Page</h1> 
+         <Header/>
+         <About/>
+      </div>
+}
+
+
